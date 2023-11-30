0169734db96a22fa32430d3c8546ac1cfb6c4754
Mamadou Barry <mamadou.barry@student.vinci.be>
2023-11-30 13:32:37 +0100

    simulation de partie

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ const corsOptions = {
 const usersRouter = require('./routes/users');
 const pizzaRouter = require('./routes/pizzas');
 const authsRouter = require('./routes/auths');
+const evoRumbleRouter = require('./routes/evoRumble');
 
 const app = express();
 
@@ -23,5 +24,6 @@ app.use(cors(corsOptions));
 app.use('/users', usersRouter);
 app.use('/pizzas', pizzaRouter);
 app.use('/auths', authsRouter);
+app.use('/evoRumble', cors(corsOptions), evoRumbleRouter);
 
 module.exports = app;
