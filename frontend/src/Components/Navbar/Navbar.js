39c21dd71e3ff91088b887c1363869d0ea9d9c9e
brian-villanueva-vinci <brian.villanueva@student.vinci.be>
2023-12-08 21:50:12 +0100

    update navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -11,47 +11,33 @@ import { Navbar as BootstrapNavbar } from 'bootstrap';
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   const navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#" data-uri="/">logo</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/game">Game</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/evoRumble"></a>
-              </li>    
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/rule">Game Rule</a>
-              </li>       
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/profil">Profil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/login">Se Connecter</a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/register">S'Enregistrer</a>
-              </li>              
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid row justify-content-end">
+        <div class="row" id="navbarSupportedContent">
+          <div class="nav-item col-4" id="divGauche">
+            <div class="nav-item p-2">
+              <a class="nav-link" href="#" data-uri="/rule">Game Rule</a>
+            </div>
+          </div>
+          <div class="nav-item col-4 text-center" id="divCentre">
+            <div class="nav-item p-2">
+              <a class="nav-link" href="#" data-uri="/">EvoRumbleLOGO</a>
+            </div>
+          </div>
+          <div class="nav-item col-4 d-flex justify-content-end" id="divDroite">
+            <div class="nav-item p-2">
+              <a class="nav-link" href="#" data-uri="/profil">Profil</a>
+            </div>
+            <div class="nav-item p-2">
+              <a class="nav-link" href="#" data-uri="/login">Se Connecter</a>
+            </div> 
+            <div class="nav-item p-2">
+              <a class="nav-link" href="#" data-uri="/register">S'Enregistrer</a>
+            </div>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   `;
   navbarWrapper.innerHTML = navbar;
 };
