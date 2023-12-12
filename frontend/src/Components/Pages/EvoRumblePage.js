37ff51100661df1879dea544f3dc82be0b1fe8f7
Mamadou Barry <mamadou.barry@student.vinci.be>
2023-12-12 16:17:38 +0100

    test git fetch

--- frontend/src/Components/Pages/EvoRumblePage.js
+++ frontend/src/Components/Pages/EvoRumblePage.js
@@ -18,7 +18,7 @@ générer  l'html après chaque click en fct du gameState
 fin de partie dès que une des équipes est vide 
 */
 
-
+// ajout changement
 import Navigate from '../Router/Navigate';
 import { clearPage } from '../../utils/render';
 
