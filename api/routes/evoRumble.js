4b374d49d18f4b95534b94e1abe30d00c759e256
Mamadou Barry <mamadou.barry@student.vinci.be>
2023-12-14 15:37:12 +0100

    Création bd pour les monstres

--- api/routes/evoRumble.js
+++ api/routes/evoRumble.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const { getAllMonsters, getMonstersByType, getAllAttacks } = require('../models/evoRumble');
+// const { getAllMonsters, getMonstersByType, getAllAttacks } = require('../models/evoRumble');
+const { getAllEvoMonsters } = require('../models/bd_evoRumble');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const allMonsters = getAllMonsters();
-  const allAttacks = getAllAttacks();
-  return res.json({ allMonsters, allAttacks });
+  // const allMonsters = getAllMonsters();
+  // const allAttacks = getAllAttacks();
+  const all = getAllEvoMonsters();
+  return res.json({ all });
 });
 
-router.get('/:type', (req, res) => {
-  const allMonsters = getMonstersByType(req.params.type);
+// router.get('/:type', (req, res) => {
+//   const allMonsters = getMonstersByType(req.params.type);
 
-  return res.json(allMonsters);
-});
+//   return res.json(allMonsters);
+// });
 
 module.exports = router;
